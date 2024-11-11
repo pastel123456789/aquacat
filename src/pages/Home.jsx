b2713f3cf42dbbc21d0a9e0674e326a103bcd1ee
pastel DeQueijo <kauan.soliveira@eaportal.org>
2024-11-11 17:21:46 +0000

    arrumando home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,15 @@ import foto from '../assets/Fotos.png';
 export function Home() {
   return (
     <div className="flex w-screen h-screen bg-azull items-center justify-center"> {/* Cor de fundo azul */}
-      <section className="flex max-lg:flex-col p-4 bg-azull items-center justify-center"> {/* Flexbox para alinhamento */}
-      <img src={foto} alt="Bebedouro automático"  className="max-lg:h-80 max-lg:w-80 lg:max-w-2xl max-h-96 object-cover -mt-80 rounded-lg" />
-        <h2 className="max-w-screen-sm ml-4 text-wht mb-80 bg-extend-fontFamily-coco-b font-lexend text-lg -mt-72"> 
+      <section className="flex flex-col p-4 bg-azull items-center justify-center"> {/* Flexbox para alinhamento */}
+        <h1 className='text-wht text-4xl mb-5 mt-5 font-lexend font-bold'>Início</h1>
+        <img src={foto} alt="Bebedouro automático"  className="mb-5 rounded-lg" />
+        <h2 className="text-wht bg-extend-fontFamily-coco-b font-lexend text-lg"> 
           Nosso produto é um bebedouro automático feito especificamente para gatos domésticos, 
           que tem como objetivo aumentar a hidratação e o conforto do gato, dando mais autonomia tanto para
-          o gato e seu dono. <a href="https://youtu.be/uL-nnhk1emY?si=pGWTsu8IMWwbnc9O"  target="_blank"  className="text-red-500 hover:text-blue-500">Veja o produto funcionando</a>
+          o gato e seu dono.
         </h2> 
+        <a href="https://youtu.be/uL-nnhk1emY?si=pGWTsu8IMWwbnc9O"  target="_blank"  className="text-red-500 hover:text-blue-500 bg-white mt-5 rounded-lg">Veja o produto funcionando</a>
       </section>
     </div>
   );
