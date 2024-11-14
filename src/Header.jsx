0c00e41e5ba88eb3bae6c4880f9ea3063d3d7fa1
pastel DeQueijo <kauan.soliveira@eaportal.org>
2024-11-14 21:06:45 +0000

    arrumando mais

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,56 @@
 import gato from './assets/logo.png'
 
+import { List, X } from "@phosphor-icons/react"
+
+import { useState } from "react"
+
+
+
 export function Header() {
+  //coloque as variáveis e a função aqui!!!
+  const [abrirMenu, setAbrirMenu] = useState(false)
+
+  function handleAbrirFecharMenu() {
+    if (abrirMenu === true) {
+      setAbrirMenu(false)
+      return
+    }
+
+    setAbrirMenu(true)
+  }
+
   return (
-    <header className="flex max-lg:flex-col items-center justify-center">
-      <div className="ml-10">
-      <img src={gato} alt="" className="max-w-sm max-lg:h-10 lg:h-40 object-cover" />
+    <header>
+      <div class="lg:hidden">
+        <div className='flex justify-between p-4'>
+          <img src={gato} alt="" className="max-w-sm max-lg:h-10 lg:h-40 object-cover" />
+          <button onClick={handleAbrirFecharMenu}>
+            {abrirMenu === true ? <X size={32} weight="bold" /> : <List size={32} weight="bold" />}
+          </button>
+        </div>
+
+        <nav className={`bg-but text-wht flex flex-col w-60 h-screen text-center gap-5 fixed right-${abrirMenu === true ? "0" : "[-15rem]"} transition-all delay-150`}>
+          <a href="/" >Início</a>
+          <a href="/Funcionamento"  >Funcionamento</a>
+          <a href="/Materiais"  >Materiais</a>
+          <a href="/Bibliografia"  >Bibliografia</a>
+          <a href="/Sobre"  >Sobre nós</a>
+          <a href="/Jogo"  >Jogos</a>
+          </nav>
       </div>
-      <div className="flex max-lg:flex-col itens-center text-wht space-x-4 text-center justify-center h-full w-full">
-        <a href="/" className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Início</a>
-        <a href="/Funcionamento"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Funcionamento</a>
-        <a href="/Materiais"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Materiais</a>
-        <a href="/Bibliografia"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Bibliografia</a>
-        <a href="/Sobre"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Sobre nós</a>
-        <a href="/Jogo"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Jogos</a>
+
+      <div class="hidden lg:block flex-col">
+        <div className="ml-10 flex items-center justify-center">
+          <img src={gato} alt="" className="max-w-sm max-lg:h-10 lg:h-40 object-cover" />
+        </div>
+        <div className="flex max-lg:flex-col itens-center text-wht space-x-4 text-center justify-center h-full w-full mb-10">
+          <a href="/" className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Início</a>
+          <a href="/Funcionamento"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Funcionamento</a>
+          <a href="/Materiais"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Materiais</a>
+          <a href="/Bibliografia"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Bibliografia</a>
+          <a href="/Sobre"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Sobre nós</a>
+          <a href="/Jogo"  className="rounded-lg bg-but text-white max-lg:w-full h-8 w-[120px] font-lexand font-bold">Jogos</a>
+        </div>
       </div>
     </header>
     
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,16 @@ import foto from '../assets/Fotos.png';
 
 export function Home() {
   return (
-    <div className="flex w-screen h-screen bg-blu items-center justify-center"> {/* Cor de fundo azul */}
-      <section className="flex flex-col p-4 bg-blu items-center justify-center"> {/* Flexbox para alinhamento */}
-        <h1 className="text-wht text-4xl mb-12 mt-10 font-lexend font-bold">Início</h1>
-        <img src={foto} alt="Bebedouro automático" className="mb-5 rounded-lg" />
-        <h2 className="text-wht bg-extend-fontFamily-coco-b font-lexend text-lg text-justify">
+    <div className="min-h-screen flex flex-col items-center w-screen bg-blu bg-cover bg-center"> {/* Cor de fundo azul */}
+        <h1 className="text-wht text-4xl mb-20 mt-11 font-lexend font-bold">Início</h1>
+        <img src={foto} alt="Bebedouro automático" className="rounded-lg" />
+        <p className="max-w-screen-sm ml-4 text-wht bg-extend-fontFamily-coco-b font-lexend text-lg mt-10">
           Nosso produto é um bebedouro automático feito especificamente para gatos domésticos, 
           que tem como objetivo aumentar a hidratação e o conforto do gato, dando mais autonomia tanto para
           o gato e seu dono.
-        </h2>
-      </section>
+        </p>
+
+         
     </div>
   );
 }
